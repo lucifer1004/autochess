c0078d4efa9dc9c4204903a9e2d2e778ccef3aa0
Gabriel Wu <qqbbnease1004@126.com>
2019-03-02 17:31:50 +0800

    fix: readd Battlefield.vue

---
+++ src/components/Battlefield.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card color="green darken-4" style="padding-bottom: 20px">
+    <v-card-title>
+      <h3 class="headline mb-0">Battlefield</h3>
+    </v-card-title>
+    <ChessRow v-for="row in rows" :key="row" :row="row" :cols="cols" />
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ChessRow from '@/components/ChessRow.vue'
+
+export default Vue.extend({
+  name: 'Battlefield',
+  components: {ChessRow},
+  props: {
+    cols: {type: Number, default: 8},
+    rows: {type: Number, default: 8},
+  },
+})
+</script>
