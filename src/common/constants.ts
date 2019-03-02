b45d0f481e23633666ca8a4cefba22946e7d4d51
Gabriel Wu <qqbbnease1004@126.com>
2019-03-02 12:05:21 +0800

    feat: add a few chesses

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,49 +3,74 @@ import {Chess} from '@/common/types'
 export const CHESSES_ONE: Chess[] = [
   {
     name: 'Axe',
-    cost: 1,
-    star: 1,
     isOrc: true,
     isWarrior: true,
   },
-]
+  {
+    name: 'DrowRanger',
+    isUndead: true,
+    isHunter: true,
+  },
+  {
+    name: 'Tiny',
+    isElemental: true,
+    isWarrior: true,
+  },
+].map(obj => Object.assign(obj, {cost: 1, star: 1}))
 
 export const CHESSES_TWO: Chess[] = [
   {
     name: 'BeastMaster',
-    cost: 2,
-    star: 1,
-    isHunter: true,
     isOrc: true,
+    isHunter: true,
   },
-]
+  {
+    name: 'Juggernaut',
+    isOrc: true,
+    isWarrior: true,
+  },
+].map(obj => Object.assign(obj, {cost: 2, star: 1}))
 
 export const CHESSES_THREE: Chess[] = [
   {
     name: 'Lycan',
-    cost: 3,
-    star: 1,
     isBeast: true,
     isHuman: true,
     isWarrior: true,
   },
-]
+  {
+    name: 'Abaddon',
+    isUndead: true,
+    isKnight: true,
+  },
+  {
+    name: 'OmniKnight',
+    isHuman: true,
+    isKnight: true,
+  },
+].map(obj => Object.assign(obj, {cost: 3, star: 1}))
 
 export const CHESSES_FOUR: Chess[] = [
   {
     name: 'Alchemist',
-    cost: 4,
-    star: 1,
+    isGoblin: true,
+    isWarlock: true,
   },
-]
+  {
+    name: 'DragonKnight',
+    isDragon: true,
+    isHuman: true,
+    isKnight: true,
+  },
+].map(obj => Object.assign(obj, {cost: 4, star: 1}))
 
 export const CHESSES_FIVE: Chess[] = [
   {
     name: 'Lich',
-    cost: 5,
-    star: 1,
+    isUndead: true,
+    isMage: true,
   },
-]
+].map(obj => Object.assign(obj, {cost: 5, star: 1}))
 
 export const CHESSES_ALL = [
   ...CHESSES_ONE,
